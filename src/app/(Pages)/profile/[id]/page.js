c2641b0e78ea65def68e2d096db37d8bf8fb6482
Andrey <super.okha2018@yandex.ru>
2023-12-20 14:29:11 +1000

    final

--- src/app/(Pages)/profile/[id]/page.js
+++ src/app/(Pages)/profile/[id]/page.js
@@ -4,36 +4,46 @@ import '@/app/Styles/profile.scss';
 import ModalPassword from "@/app/Components/ModalPassword";
 import { useDispatch, useSelector } from 'react-redux';
 import { me, logout } from '@/app/Redux/features/auth/authSlice';
+import axios from 'axios';  // Добавим этот импорт
 import Link from "next/link";
 export default function Profile() {
   const [isModalOpenPassword, setIsModalOpenPassword] = useState(false);
+  const [data, setData] = useState([]);
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.auth.ID);
-  console.log(userId)
+  const [username, setUsername] = useState('');
 
-  const [username, setUsername] = useState(""); // Добавляем состояние для имени пользователя
 
   useEffect(() => {
-    const fetchUserData = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(`http://74.119.192.138:5000/api/users/one-user/${userId}`);
-        const userData = await response.json();
+        const userResponse = await fetch(`http://74.119.192.138:5000/api/users/one-user/${userId}`);
+        const userData = await userResponse.json();
+  
         if (userData.length > 0) {
           setUsername(userData[0].username);
+  
+          const rolesResponse = await axios.get('http://74.119.192.138:5000/api/role/all-roles');
+          const userRoles = rolesResponse.data.filter((role) => role.UserName === userData[0].username);
+          
+          if (userRoles.length > 0) {
+            setData(userRoles.map((role) => ({ name: role.ProjectName, role: role.Role, status: 'В разработке' })));
+            
+          } 
+        
         }
       } catch (error) {
-        console.error('Ошибка при получении данных пользователя:', error);
+        console.log('Ошибка при получении данных:', error);
       }
     };
-
+  
     if (userId) {
-      fetchUserData();
+      fetchData();
     }
   }, [userId]);
   const handleLogout = async () => {
     try {
       await dispatch(logout());
-     
     } catch (error) {
       console.error('Logout error:', error);
     }
@@ -47,39 +57,38 @@ export default function Profile() {
     setIsModalOpenPassword(false);
   };
 
-  const data = [
-    { name: "NameProject", role: "Участник", status: "В разработке" },
-    { name: "NameProject", role: "Участник", status: "В разработке" },
-    { name: "NameProject", role: "Участник", status: "В разработке" },
-    { name: "NameProject", role: "Участник", status: "В разработке" },
-  ];
-
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
     <main>
-      <ModalPassword isOpen={isModalOpenPassword} onClose={closeModalPassword} />
-      <div className="Profile">
-        <h1>Портфолио</h1>
-        <Link href="/"><input type="button" onClick={handleLogout} value={"Выйти"} /> </Link>
-        <div className="Profile__search">
-          <div className="Profile__search__input">
-            <input type="text" placeholder="Поиск" />
-            <img src="/Lupa.png" alt="Search" />
+    <ModalPassword isOpen={isModalOpenPassword} onClose={closeModalPassword} />
+    <div className="Profile">
+      <h1>Портфолио</h1>
+      <Link href="/">
+        <input type="button" onClick={handleLogout} value={'Выйти'} />
+      </Link>
+      <div className="Profile__search">
+        <div className="Profile__search__input">
+          <input type="text" placeholder="Поиск" />
+          <img src="/Lupa.png" alt="Search" />
+        </div>
+      </div>
+
+      <div className="Profile__container">
+        <div className="Profile__container__user">
+          <div className="Profile__container__user__avatar">
+            <img src="/pencil.png" alt="Avatar" />
+          </div>
+          <div className="Profile__container__user__login">
+            <p>{username}</p>
+            <input type="button" onClick={openModalPassword} value="Изменить пароль" />
           </div>
         </div>
 
-        <div className="Profile__container">
-          <div className="Profile__container__user">
-            <div className="Profile__container__user__avatar">
-              <img src="/pencil.png" alt="Avatar" />
-            </div>
-            <div className="Profile__container__user__login">
-              <p>{username}</p>
-              <input type="button" onClick={openModalPassword} value="Изменить пароль" />
-            </div>
-          </div>
-
-          <div className="Profile__container__project">
-            {data.map((item, index) => (
+        <div className="Profile__container__project">
+          {data.length > 0 ? (
+            data.map((item, index) => (
               <div className="Profile__container__project__item" key={index}>
                 <div className="Profile__container__project__item__number">
                   <p>{index + 1}</p>
@@ -90,14 +99,16 @@ export default function Profile() {
                   </div>
                   <div className="Profile__container__project__item__name__role">
                     <p>{item.role}</p>
-                    <p>{item.status}</p>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            ))
+          ) : (
+            <p>Проектов нет</p>
+          )}
         </div>
       </div>
-    </main>
+    </div>
+  </main>
   );
 }
